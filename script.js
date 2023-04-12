a5dd0f7a82ea25683698b15560ea9afaeab66663
pepise <alperencanesen@gmail.com>
2023-04-13 02:58:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function login() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    if (username == "" || password == "") {
-      alert("Lütfen kullanıcı adı ve şifrenizi girin.");
-      return false;
-    } else if (username != "kullaniciadi" || password != "sifre123") {
-      alert("Kullanıcı adı veya şifre yanlış.");
-      return false;
-    } else {
-      window.location.href = "anasayfa.html";
-    }
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username == "" || password == "") {
+    alert("Lütfen kullanıcı adı ve şifrenizi girin.");
+    return false;
+  } else {
+    // JSON veritabanını yükle
+    fetch("users.json")
+      .then(response => response.json())
+      .then(data => {
+        var userExists = false;
+
+        // Kullanıcı adı ve şifreyi kontrol et
+        data.users.forEach(user => {
+          if (user.username == username && user.password == password) {
+            userExists = true;
+            return;
+          }
+        });
+
+        if (userExists) {
+          window.location.href = "anasayfa.html";
+        } else {
+          alert("Kullanıcı adı veya şifre yanlış.");
+          return false;
+        }
+      })
+      .catch(error => console.error(error));
   }
-  
+}
