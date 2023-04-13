8b409be9d3843b952b3c8af8abb732deb8d5d76f
pepise <alperencanesen@gmail.com>
2023-04-13 03:15:19 +0300

    deneme1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function login() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
@@ -30,3 +31,60 @@ function login() {
       .catch(error => console.error(error));
   }
 }
+function register() {
+  var ad = document.getElementById("ad").value;
+  var soyad = document.getElementById("soyad").value;
+  var kullaniciadi = document.getElementById("kullaniciadi").value;
+  var sifre = document.getElementById("sifre").value;
+  var eposta = document.getElementById("eposta").value;
+
+  // Yeni kullanıcı oluştur
+  var newUser = {
+    "ad": ad,
+    "soyad": soyad,
+    "username": kullaniciadi,
+    "password": sifre,
+    "email": eposta
+  };
+
+  // Kullanıcıları yükle
+  fetch("kullanicilar.json")
+    .then(response => response.json())
+    .then(data => {
+      // Yeni kullanıcı adı mevcut mu diye kontrol et
+      var usernameExists = false;
+      data.users.forEach(user => {
+        if (user.username == kullaniciadi) {
+          usernameExists = true;
+          return;
+        }
+      });
+      if (usernameExists) {
+        alert("Bu kullanıcı adı zaten mevcut.");
+        return false;
+      } else {
+        // Yeni kullanıcıyı ekle
+        data.users.push(newUser);
+
+        // Yeni kullanıcıyı kaydet
+        var jsonData = JSON.stringify(data);
+        fetch("kullanicilar.json", {
+          method: "PUT",
+          body: jsonData,
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            alert("Kayıt başarılı!");
+            window.location.href = "index.html";
+          })
+          .catch(error => console.error(error));
+      }
+    })
+    .catch(error => console.error(error));
+}
+
+
+      // Eğer kullanıcı adı mevcut değilse, yeni kullanıcıyı e
